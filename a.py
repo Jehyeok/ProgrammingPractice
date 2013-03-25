1e244fc4d624222d15418d72ba36dad074f75ead
jehyeok <hook3748@gmail.com>
2013-03-25 11:28:59 +0900

    exercise1

---
+++ a.py
@@ -0,0 +1,9 @@
+def print_name(name1) :
+	print "your name : "
+	print name1
+
+def print_rename(name1, name2) :
+	print_name(name1)
+	print 'he is from',name2,'family'
+
+print_rename('bruce', 'Lee')
